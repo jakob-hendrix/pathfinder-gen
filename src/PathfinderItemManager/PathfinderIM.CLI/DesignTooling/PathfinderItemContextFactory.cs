7acdee95e92c47ba5cdf74b0a235d31e5eff080c
jakob-hendrix <jhendrix001@gmail.com>
2020-03-20 22:02:00 -0500

    Solved issue with the ef migrations.
    Had to back down a version and add a design time class to my console app, which sucks

---
+++ src/PathfinderItemManager/PathfinderIM.CLI/DesignTooling/PathfinderItemContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using PathfinderIM.CLI.Services;
+using PathfinderIM.DataAccess;
+
+namespace PathfinderIM.CLI.DesignTooling
+{
+    public class PathfinderItemContextFactory : IDesignTimeDbContextFactory<PathfinderItemContext>
+    {
+        public PathfinderItemContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationService().GetConfiguration();
+            var builder = new DbContextOptionsBuilder<PathfinderItemContext>();
+            var context = new PathfinderItemContext(builder.UseSqlite(
+                config.GetConnectionString(nameof(PathfinderItemContext)), 
+                options => options.MigrationsAssembly("PathfinderIM.DataAccess")).Options);
+            return context;
+        }
+    }
+}
